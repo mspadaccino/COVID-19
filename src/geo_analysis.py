d35d472300420b847fe824d1afa1994962a3f688
Maurizio Spadaccino <maurizio.spadaccino@illimity.com>
2020-04-08 11:42:47 +0200

    creating functions for place downloads

--- src/geo_analysis.py
+++ src/geo_analysis.py
@@ -3,14 +3,22 @@ import urllib.request as url_req
 import time
 import pandas as pd
 import populartimes
-
+import tqdm
 with open ('api_key.cfg') as file:
     API_KEY = file.readline().strip()
 
-NATAL_CENTER = (45.4654219, 9.1859243) # Milan Coordinates
+with open('../data/geodata/places_types.config') as f:
+    places_types = f.readlines()
+    places_types = [item.replace('\n','') for item in places_types]
+
+
+MILAN_CENTER = (45.4654219, 9.1859243) # Milan Coordinates
+bounding_box_milan_lower = [45.390368, 9.092639]
+bounding_box_milan_upper = [45.539202,9.284036]
 
 API_NEARBY_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
 RADIUS = 30000
+
 PLACES_TYPES = [('airport', 1), ('bank', 2), ('bar', 3), ('beauty_salon', 3), ('book_store', 1), ('cafe', 1), ('church', 3), ('doctor', 3), ('dentist', 2), ('gym', 3), ('hair_care', 3), ('hospital', 2), ('pharmacy', 3), ('pet_store', 1), ('night_club', 2), ('movie_theater', 1), ('school', 3), ('shopping_mall', 1), ('supermarket', 3), ('store', 3)]
 
 def request_api(url):
@@ -20,7 +28,7 @@ def request_api(url):
     return json_data
 
 def get_places(types, pages):
-    location = str(NATAL_CENTER[0]) + "," + str(NATAL_CENTER[1])
+    location = str(MILAN_CENTER[0]) + "," + str(MILAN_CENTER[1])
     mounted_url = ('%s'
                    '?location=%s'
                    '&radius=%s'
@@ -63,15 +71,12 @@ def parse_place_to_list(place, type_name):
     ]
 
 
-def mount_dataset():
+def update_places_dataset():
     data = []
-
     for place_type in PLACES_TYPES:
         type_name = place_type[0]
         type_pages = place_type[1]
-
         print("Getting into " + type_name)
-
         result = get_places(type_name, type_pages)
         result_parsed = list(map(lambda x: parse_place_to_list(x, type_name), result))
         data += result_parsed
@@ -81,37 +86,57 @@ def mount_dataset():
     return dataframe
 
 
-def get_place_popular_moments(place_id):
-    popular_moments = populartimes.get_id(API_KEY, place_id)
-    if 'populartimes' in popular_moments:
-        return popular_moments['populartimes']
-    else:
-        return None
+def get_place_details(place_id):
+    poptimes = populartimes.get_id(API_KEY, place_id)
+    return pd.json_normalize(poptimes)
+
+
+def get_places_popularity(places):
+    places_popularity = pd.DataFrame()
+    for row, item in tqdm.tqdm(places.iterrows()):
+        places_popularity = places_popularity.append(get_place_details(item.place_id), ignore_index=True)
+    places_popularity.to_csv('places_popularity.csv')
+    return places_popularity
 
 
 if __name__ == '__main__':
-    places = mount_dataset()
-    places = pd.read_csv('../data/places.csv', index_col=0)
-    places['monday'] = None
-    places['tuesday'] = None
-    places['wednesday'] = None
-    places['thursday'] = None
-    places['friday'] = None
-    places['saturday'] = None
-    places['sunday'] = None
-    for (index, row) in places.iterrows():
-        print("Populating " + str(index))
-        moments = get_place_popular_moments(row.place_id)
-        if moments != None:
-            places.at[index, 'monday'] = moments[0]['data']
-            places.at[index, 'tuesday'] = moments[1]['data']
-            places.at[index, 'wednesday'] = moments[2]['data']
-            places.at[index, 'thursday'] = moments[3]['data']
-            places.at[index, 'friday'] = moments[4]['data']
-            places.at[index, 'saturday'] = moments[5]['data']
-            places.at[index, 'sunday'] = moments[6]['data']
 
-    places.to_csv('../data/places_with_moments.csv')
-    print(places)
-    # row = places.iloc[0]
-    # check = get_place_popular_moments(row.place_id)
+    # test = populartimes.get(API_KEY, ['bank', 'hotel'], bounding_box_milan_lower, bounding_box_milan_upper, 1,radius=100)
+
+    UPDATE_PLACES_DATASET = False
+    if UPDATE_PLACES_DATASET:
+        places = update_places_dataset()
+    else:
+        places = pd.read_csv('../data/places.csv', index_col=0)
+
+    GET_POPULARITY = False
+    if GET_POPULARITY:
+        places_popularity = get_places_popularity(places)
+    else:
+        places_popularity = pd.read_csv('../data/places_popularity.csv', index_col=0)
+
+        # places = pd.read_csv('../data/places.csv', index_col=0)
+        # places['monday'] = None
+        # places['tuesday'] = None
+        # places['wednesday'] = None
+        # places['thursday'] = None
+        # places['friday'] = None
+        # places['saturday'] = None
+        # places['sunday'] = None
+        # places['current'] = None
+        # for (index, row) in places.iterrows():
+        #     print("Populating " + str(index))
+        #     moments = get_place_popular_moments(row.place_id)
+        #     if moments != None:
+        #         places.at[index, 'monday'] = moments[0]['data']
+        #         places.at[index, 'tuesday'] = moments[1]['data']
+        #         places.at[index, 'wednesday'] = moments[2]['data']
+        #         places.at[index, 'thursday'] = moments[3]['data']
+        #         places.at[index, 'friday'] = moments[4]['data']
+        #         places.at[index, 'saturday'] = moments[5]['data']
+        #         places.at[index, 'sunday'] = moments[6]['data']
+        #         places.at[index, 'sunday'] = moments[6]['data']
+        #
+        # places.to_csv('../data/places_with_moments.csv')
+        # print(places)
+
