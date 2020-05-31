13a62ebd21a0faf12c08b9953e54da68824e3bf5
maurizio <mspadaccino@gmail.com>
2020-05-31 07:17:09 +0200

    added italy sub tabs

--- dash/covid_dashboard.py
+++ dash/covid_dashboard.py
@@ -41,52 +41,69 @@ app.layout = html.Div(children=[
                                                 html.P('''Select one of the analysis from below.'''),
                                                 # Dividing the dashboard in tabs
                                                 dcc.Tabs(
-                                                    id="tabs",
+                                                    id="main-tabs",
                                                     children=[# Defining the layout of the first Tab
                                                         dcc.Tab(
                                                             label='Italy',
                                                             children=[
-                                                                dcc.Dropdown(
-                                                                    id='region',
-                                                                    options=get_options(df_reg.keys()),
-                                                                    # [{'label': value, 'value': value} for value in df_reg.keys()],
-                                                                    value='Italy',
-                                                                    clearable=False,
-                                                                    multi=False
-                                                                ),
-                                                                dcc.Dropdown(
-                                                                    id='label',
-                                                                    options=[{'label': value, 'value': value} for value
-                                                                             in df_reg['Italy'].columns],
-                                                                    value='nuovi_positivi',
-                                                                    clearable=False,
-                                                                    multi=False
-                                                                ),
-                                                                dcc.DatePickerSingle(
-                                                                    id='start_fit',
-                                                                    min_date_allowed=pd.to_datetime(df_naz.index[0]),
-                                                                    max_date_allowed=pd.to_datetime(df_naz.index[-1]),
-                                                                    date=pd.to_datetime(df_naz.index[0])
-                                                                ),
-                                                                dcc.DatePickerSingle(
-                                                                    id='end_fit',
-                                                                    min_date_allowed=pd.to_datetime(df_naz.index[0]),
-                                                                    max_date_allowed=pd.to_datetime(df_naz.index[-1]),
-                                                                    date=pd.to_datetime(df_naz.index[-1])
-                                                                ),
-                                                                dcc.Input(
-                                                                    id='forecast_periods',
-                                                                    value=90,
-                                                                    # type= 'int',
-                                                                    placeholder='forecasting period',
-                                                                ),
-                                                                dcc.Input(
-                                                                    id='smoothing',
-                                                                    value=1,
-                                                                    # type='int',
-                                                                    placeholder='smoothing days',
+                                                                dcc.Tabs(
+                                                                    id="italy-tabs",
+                                                                    children=[
+                                                                        dcc.Tab(
+                                                                            label='trend',
+                                                                            children=[
+                                                                                dcc.Dropdown(
+                                                                                    id='region',
+                                                                                    options=get_options(df_reg.keys()),
+                                                                                    # [{'label': value, 'value': value} for value in df_reg.keys()],
+                                                                                    value='Italy',
+                                                                                    clearable=False,
+                                                                                    multi=False
+                                                                                ),
+                                                                                dcc.Dropdown(
+                                                                                    id='label',
+                                                                                    options=[{'label': value, 'value': value}
+                                                                                             for value
+                                                                                             in df_reg['Italy'].columns],
+                                                                                    value='nuovi_positivi',
+                                                                                    clearable=False,
+                                                                                    multi=False
+                                                                                ),
+                                                                                dcc.DatePickerSingle(
+                                                                                    id='start_fit',
+                                                                                    min_date_allowed=pd.to_datetime(
+                                                                                        df_naz.index[0]),
+                                                                                    max_date_allowed=pd.to_datetime(
+                                                                                        df_naz.index[-1]),
+                                                                                    date=pd.to_datetime(df_naz.index[0])
+                                                                                ),
+                                                                                dcc.DatePickerSingle(
+                                                                                    id='end_fit',
+                                                                                    min_date_allowed=pd.to_datetime(
+                                                                                        df_naz.index[0]),
+                                                                                    max_date_allowed=pd.to_datetime(
+                                                                                        df_naz.index[-1]),
+                                                                                    date=pd.to_datetime(df_naz.index[-1])
+                                                                                ),
+                                                                                dcc.Input(
+                                                                                    id='forecast_periods',
+                                                                                    value=90,
+                                                                                    # type= 'int',
+                                                                                    placeholder='forecasting period',
+                                                                                ),
+                                                                                dcc.Input(
+                                                                                    id='smoothing',
+                                                                                    value=1,
+                                                                                    # type='int',
+                                                                                    placeholder='smoothing days',
+                                                                                )
+                                                                            ]
+                                                                        )
+                                                                    ]
                                                                 )
                                                             ]
+
+
                                                         ),
                                                         dcc.Tab(
                                                             label='World',
